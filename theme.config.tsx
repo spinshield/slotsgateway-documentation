64e4f498e6b2caeb34addacb51f7234f0f89937b
spinshield <146870398+spinshield@users.noreply.github.com>
2025-12-11 22:23:56 +0100

    Fix logo spelling in theme configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>SLotsgateway API</span>,
+  logo: <span>Slotsgateway API</span>,
   darkMode: false,
   chat: {
     link: 'https://t.me/slotsgateway',
